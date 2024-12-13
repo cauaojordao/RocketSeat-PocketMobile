3ae4d97ddff681d98fdc7e645d96abe38fb17047
cauaojordao <cauaojordao@gmail.com>
2024-12-13 17:13:02 -0300

    primeira aula

--- setup/src/components/Welcome/index.tsx
+++ setup/src/components/Welcome/index.tsx
@@ -5,7 +5,7 @@ import { s } from "./styles";
 export const Welcome = () => {
     return <View>
         <Image source={require("@/assets/logo.png")} style={s.logo}/>
-        <Text style={s.title}>Bem-vindo ao</Text>
-        <Text style={s.subtitle}>Organizze</Text>
+        <Text style={s.title}>Bem-vindo ao Nearby!</Text>
+        <Text style={s.subtitle}>Tenha cupons de vantagem para usar em seus estabelecimentos favoritos.</Text>
     </View>;
 };
